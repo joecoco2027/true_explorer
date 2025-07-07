e12c7948d9ced507e7489568c5ba09df8ff142cb
joecoco2027 <joehacker9273@proton.me>
2025-07-07 06:44:15 -0400

    Fixed props

--- src/app/diaries/[slug]/page.tsx
+++ src/app/diaries/[slug]/page.tsx
@@ -1,41 +1,40 @@
-import dynamic from "next/dynamic";
-import { getDiariesMetadata } from "../_lib/getDiariesMetaData";
-import type { Metadata } from "next/types";
-import { getAllDiariesData } from "../_lib/getAllDiariesData";
-import markdownStyles from "@/app/_components/markdown/markdown.module.css";
-import Image from "next/image";
+import type { Metadata } from "next";
 
 export async function generateMetadata({
-    params,
-}: BlogPageProps) : Promise<Metadata> {
-    const { metadata } = await getDiariesMetadata(params.slug);
-    if (metadata) {
-        return metadata;
-    } else {
-        throw new Error(`No Metadata Found for blog post: ${params.slug}`);
-    }
-}
-export async function generateStaticParams(){
-    const blogPosts = await getAllDiariesData();      //FIXME: Read from file System
-    const blogStaticParams = blogPosts.map((post) => ({
-        slug: post.slug,
-    }));
-    return blogStaticParams;
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata> {
+  const { metadata } = await getDiariesMetadata(params.slug);
+  if (metadata) {
+    return metadata;
+  }
+  throw new Error(`No Metadata Found for blog post: ${params.slug}`);
 }
 
+export async function generateStaticParams() {
+  const blogPosts = await getAllDiariesData();
+  return blogPosts.map((post) => ({
+    slug: post.slug,
+  }));
+}
 
-type BlogPageProps = { params : {
-    slug: string 
-}}; 
-
-export default async function DiariesPage({ params } : BlogPageProps){
-    const { metadata } = await getDiariesMetadata(params.slug);
-    const cover = `${metadata.cover ?? ""}`
-    const DiariesMarkdown = dynamic(() => import("@/diaries/" + params.slug + ".mdx"));
-    return (
-        <div className={`prose lg:prose-xl dark:prose-invert container mx-auto p-4 ${markdownStyles.markdown} mx-auto px-4 flex flex-col justify-center align-middle self-center w-200 text-center items-center`}>
-            <Image src={cover} height={200} width={200} alt="cover image"/>
-            <DiariesMarkdown />
-        </div>
-    );
+export default async function DiariesPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const { metadata } = await getDiariesMetadata(params.slug);
+  const cover = metadata.cover ?? "";
+  const DiariesMarkdown = dynamic(() =>
+    import(`@/diaries/${params.slug}.mdx`)
+  );
+  return (
+    <div
+      className={`prose lg:prose-xl dark:prose-invert container mx-auto p-4 ${markdownStyles.markdown} mx-auto px-4 flex flex-col justify-center align-middle self-center w-200 text-center items-center`}
+    >
+      <Image src={cover} height={200} width={200} alt="cover image" />
+      <DiariesMarkdown />
+    </div>
+  );
 }
