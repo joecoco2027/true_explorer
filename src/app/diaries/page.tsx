eaf03b634d4e3fd344187d3e488e084454fb6418
joecoco2027 <joehacker9273@proton.me>
2025-07-08 14:13:53 -0400

    Added about

--- src/app/diaries/page.tsx
+++ src/app/diaries/page.tsx
@@ -8,11 +8,11 @@ export default async function Blogs(){
             <h1 className="my-4 text-center text-teal-800 text-9xs font-bold ">Diaries</h1>
             <p>Here are some recent Diaries.</p>
             <ul className="px-5 ml-4 flex flex-1/2 flex-wrap">
-                {blogs.map(({ slug, metadata: { title, cover }}) => (
+                {blogs.map(({ slug, metadata: { title}}) => (
                     <li key={slug} className="p-5 hover:scale-110">
                             <Link prefetch={false} href={`/diaries/${slug}`}>
                                 <h1>{`${title}`}</h1>
-                                <Image src={cover} width={100} height={100} alt="Cover Image"/>
+                                <Image src="/images/diaries/diaries3.jpg" width={400} height={400} alt="Cover Image" className="rounded-2xl"/>
                             </Link>
                     </li> 
                 ))}
